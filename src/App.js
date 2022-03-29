39cbf1df77f0cfd48dcd85ee672df1e251d7dc96
Eric Ho <endonoho@gmail.com>
2022-03-28 21:31:30 -0700

    Implement initial app styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Grid } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
 import useAppData from './hooks/useAppData';
+
+const useStyles = makeStyles(theme => ({
+	root: {
+		margin: '0.5rem',
+		padding: '1rem',
+		'&.MuiGrid-container': {
+			justifyContent: 'center',
+		},
+	},
+}));
+
 
 function App() {
 	const { state } = useAppData();
 	const { studentAssessments } = state;
+	const classes = useStyles();
 
   return (
-    <div className="App">
-
-    </div>
+    <Grid container direction='column' className={classes.root}>
+    </Grid>
   );
 }
 
